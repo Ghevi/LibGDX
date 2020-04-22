3952e0f6eb2c0e35e33a4727344bcec468ee6112
Ghevi <ghevi96@gmail.com>
2020-04-22 18:21:00 +0200

    Initial commit
     Changes to be committed:
    	new file:   android/AndroidManifest.xml
    	new file:   android/assets/Game BG.png
    	new file:   android/assets/badlogic.jpg
    	new file:   android/build.gradle
    	new file:   android/ic_launcher-web.png
    	new file:   android/proguard-rules.pro
    	new file:   android/project.properties
    	new file:   android/res/drawable-anydpi-v26/ic_launcher.xml
    	new file:   android/res/drawable-anydpi-v26/ic_launcher_foreground.xml
    	new file:   android/res/drawable-hdpi/ic_launcher.png
    	new file:   android/res/drawable-mdpi/ic_launcher.png
    	new file:   android/res/drawable-xhdpi/ic_launcher.png
    	new file:   android/res/drawable-xxhdpi/ic_launcher.png
    	new file:   android/res/drawable-xxxhdpi/ic_launcher.png
    	new file:   android/res/values/color.xml
    	new file:   android/res/values/strings.xml
    	new file:   android/res/values/styles.xml
    	new file:   android/src/com/ghevi/mylibgdxtester/AndroidLauncher.java
    	new file:   build.gradle
    	new file:   core/build.gradle
    	new file:   core/src/com/ghevi/mylibgdxtester/GameMain.java
    	new file:   core/src/helpers/GameInfo.java
    	new file:   desktop/build.gradle
    	new file:   desktop/src/com/ghevi/mylibgdxtester/desktop/DesktopLauncher.java
    	new file:   gradle.properties
    	new file:   gradle/wrapper/gradle-wrapper.jar
    	new file:   gradle/wrapper/gradle-wrapper.properties
    	new file:   gradlew
    	new file:   gradlew.bat
    	new file:   settings.gradle

---
+++ desktop/src/com/ghevi/mylibgdxtester/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.ghevi.mylibgdxtester.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ghevi.mylibgdxtester.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.width = 480;
+		config.height = 800;
+
+		new LwjglApplication(new GameMain(), config);
+	}
+}
